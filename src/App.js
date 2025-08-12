90c44f930b45e090b48d88c9df9ccbd9cf5f73a0
Evgenii Yasyk <moneys@gmail.com>
2025-08-12 15:08:21 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './style/App.css';
-import {Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 import UserRegister from './components/UserRegister';
 import { UserProvider } from './components/UserContext';
@@ -7,10 +7,14 @@ import { UserProvider } from './components/UserContext';
 function App() {
   return (
     <UserProvider>
-      <Routes>
-        <Route path="/" element={<UserRegister />} />
-        <Route path="/chat" element={<ChatPage />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserRegister />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </Router>
     </UserProvider>
   );
 }
+
+export default App;
