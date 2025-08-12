53151db603eae49acec6d447d3d88fe22cea5e53
Evgenii Yasyk <moneys@gmail.com>
2025-08-12 14:59:59 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import './style/App.css';
+import {Router, Routes, Route } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import UserRegister from './components/UserRegister';
+import { UserProvider } from './components/UserContext';
 
 function App() {
   return (
-    <Router>
+    <UserProvider>
       <Routes>
-        <Route path="/" element={<div>Главная</div>} />
-        <Route path="/chat" element={<div>Чат</div>} />
+        <Route path="/" element={<UserRegister />} />
+        <Route path="/chat" element={<ChatPage />} />
       </Routes>
-    </Router>
+    </UserProvider>
   );
 }
-
-export default App;
-
